14b2fea7633e7145674683dceb04c4e680c62547
tortaruga <jojoway77@gmail.com>
2025-09-13 14:53:15 +0200

    add regular and dark theme

--- app.js
+++ app.js
@@ -21,51 +21,88 @@ const forecastElement = document.querySelector('.forecast');
 const phaseIconElement = document.querySelector('.phase-icon');
 const chanceRainElement = document.querySelector('.chance-rain');
 const chanceSnowElement = document.querySelector('.chance-snow'); 
+let img;
 
 const hourlyForecastContainer = document.querySelector('.hourly .container');
 
-let isDay = true;
+let isDay = true; 
 
 let city = 'rome';
 
-let unit = 'c'; 
+let unit = 'metric'; 
+
+let identifiers = {
+    temp: 'c',
+    speed: 'kph',
+    volume: 'mm'
+} 
 
 unitsMenu.addEventListener('change', (e) => {
     unit = e.target.value;
+
     if (!unit) {
-        unit = 'c';
+        unit = 'metric';
     }; 
+
+    handleIdentifier();
     fetchData(); 
 });
 
-function handleUnit(temp) {
-    return unit === 'c' ? `${temp} °C` : `${temp} F`; 
+function handleIdentifier() {
+    if (unit === 'metric') {
+        identifiers = {
+            temp: 'c',
+            speed: 'kph',
+            volume: 'mm'
+        }
+    } else {
+        identifiers = {
+            temp: 'f',
+            speed: 'mph',
+            volume: 'in'
+        }
+    }
+}  
+
+
+function handleUnit(type, value) {
+    let result;
+
+    if (type === 'temp') {
+        result = unit === 'metric' ? `${value} °C` : `${value} F`;
+    } else if (type === 'volume') {
+        result = unit === 'metric' ? `${value} mm` : `${value} in`;
+
+    } else if (type === 'speed') {
+        result = unit === 'metric' ? `${value} kph` : `${value} mph`;
+    }
+    return result; 
 }
 
 function fetchData() {
-    fetch(`/.netlify/functions/fetchWeather?city=${encodeURIComponent(city)}`)
+    fetch(`/.netlify/functions/fetchWeather?city=${encodeURIComponent(city)}`) 
     .then(response => response.json())
     .then(data => { 
  
         handleImage(data.current.condition.code, data.current['is_day']); 
     
         locationElement.textContent = `${data.location.name}, ${data.location.country}`;
-        
-        tempElement.textContent = handleUnit(data.current[`temp_${unit}`]);  
-         
+    
+        tempElement.innerHTML = `<span><img src=${handleImage()} class="w-[4rem] md:w-[8rem] inline pr-3" alt="weather icon" /></span>` + handleUnit('temp', data.current[`temp_${identifiers.temp}`]);   
+           
         dateElement.textContent = getFormattedDate(data.location.localtime); 
-        timeElement.textContent = getFormattedTime(data.location.localtime); 
+        timeElement.textContent = getFormattedTime(data.location.localtime);  
         // conditionElement.textContent = data.forecast.forecastday[0].day.condition.text; 
         conditionElement.textContent = data.current.condition.text; 
 
         chanceRainElement.textContent = data.forecast.forecastday[0].day["daily_chance_of_rain"] + '%';
         chanceSnowElement.textContent = data.forecast.forecastday[0].day["daily_chance_of_snow"] + '%';
 
-        feelsLikeElement.textContent = handleUnit(data.current[`feelslike_${unit}`]); 
-        precipitationsElement.textContent = data.current.precip_mm; 
-        windElement.textContent = data.current.wind_kph;
+        feelsLikeElement.textContent = handleUnit('temp', data.current[`feelslike_${identifiers.temp}`]); 
+        precipitationsElement.textContent = handleUnit('volume', data.current[`precip_${identifiers.volume}`]);
+        windElement.textContent = handleUnit('speed', data.current[`wind_${identifiers.speed}`]);;
         humidityElement.textContent = data.current.humidity + '%';
-
+ 
         sunriseElement.textContent = data.forecast.forecastday[0].astro.sunrise; 
         sunsetElement.textContent = data.forecast.forecastday[0].astro.sunset; 
         moonriseElement.textContent = data.forecast.forecastday[0].astro.moonrise; 
@@ -74,11 +111,7 @@ function fetchData() {
         displayMoonPhaseIcon(data.forecast.forecastday[0].astro.moon_phase);
         illuminationElement.textContent = data.forecast.forecastday[0].astro.moon_illumination + '%';  
 
-        // "2025-09-09 00:00" 
-        const hourNow = new Date().getHours();
         createDailyHourElements(data.forecast.forecastday[0].hour)
-        // from hourNow to 23
-        // from now to 23 create n divs displaying hour, icon, temp
     }); 
 } 
  
@@ -88,17 +121,18 @@ function createDailyHourElements(data) {
     
     for (let i = hourNow; i < 24; i++) {
         const div = document.createElement('div');
-        div.classList.add("border", "rounded-md", "bg-black", "p-4");   
+        div.classList.add("rounded-md", "bg-sky-800/10", "p-4", "text-center", "dark:bg-sky-200/10");     
         hourlyForecastContainer.appendChild(div);
 
         const hour = document.createElement('p');
         hour.textContent = getFormattedTime(data[i].time);
         div.appendChild(hour); 
         const icon = document.createElement('img');
+        icon.classList.add("w-[2rem]", "d-block", "m-auto");       
         icon.src = data[i].condition.icon; 
         div.appendChild(icon);  
         const temp = document.createElement('p');
-        temp.textContent =  handleUnit(data[i][`temp_${unit}`]);   
+        temp.textContent =  handleUnit('temp', data[i][`temp_${identifiers.temp}`]);   
         div.appendChild(temp);
     }
 
@@ -136,27 +170,27 @@ weatherForm.addEventListener('submit', (e) => {
     fetchData();
 })
 
-const now = new Date();
-if (now.getHours() >= 17 || now.getHours() < 6) {
-    document.querySelector('body').classList.add('dark');
-} else {
-    document.querySelector('body').classList.remove('dark'); 
-}  
 
-let img;
+function isNight() {
+    const now = new Date().getHours();
+    return now >= 17 || now < 6;
+}
+ 
+document.documentElement.classList.toggle('dark', isNight()); 
+ 
 function handleImage(conditionCode, isDay) { 
     if (conditionCode === 1000) {
-        img = isDay ? 'url(./images/clear.jpg)' : 'url(./images/night.jpg)';  
+        img = isDay ? '/images/clear.png' : '/images/night.png';  
     } else if (conditionCode === 1237 || conditionCode === 1261) {
-        img = 'url(./images/rainy.jpg)';
+        img = '/images/rainy.png';
     } else if (conditionCode === 1276 || conditionCode === 1282) {
-        img = 'url(./images/stormy.jpg)';
+        img = '/images/stormy.png';
     } else if (conditionCode === 1003 || conditionCode === 1006 || conditionCode === 1009) {
-        img = 'url(./images/cloudy.jpg)';
+        img = '/images/cloudy.png';
     } else if (conditionCode === 1147 || conditionCode === 1030 || conditionCode === 1135) {
-        img = 'url(./images/foggy.jpg)';
+        img = '/images/foggy.png';
     } else if (conditionCode === 1087 || conditionCode === 1273 || conditionCode === 1279) {
-        img = 'url(./images/stormy.jpg)';
+        img = '/images/stormy.png';
     } else if (conditionCode === 1171 ||  
             conditionCode === 1186 ||
             conditionCode === 1189 ||
@@ -165,7 +199,7 @@ function handleImage(conditionCode, isDay) {
             conditionCode === 1202 ||
             conditionCode === 1243 ||
             conditionCode === 1246) {
-                img = 'url(./images/rainy.jpg)';
+                img = '/images/rainy.png';
         } else if (conditionCode === 1198 ||
             conditionCode === 1240 ||
             conditionCode === 1168 ||
@@ -176,7 +210,7 @@ function handleImage(conditionCode, isDay) {
             conditionCode === 1183 ||
             conditionCode === 1153
         ) {
-            img = 'url(./images/rainy.jpg)';
+            img = '/images/rainy.png';
         } else if (conditionCode === 1255 ||
             conditionCode === 1258 ||
             conditionCode === 1225 ||
@@ -187,7 +221,7 @@ function handleImage(conditionCode, isDay) {
             conditionCode === 1114 ||
             conditionCode === 1066 
         ) {
-            img= 'url(./images/snowy.jpg)';
+            img= '/images/snowy.png';
         } else if (conditionCode === 1207 ||
             conditionCode === 1069 ||
             conditionCode === 1204 ||
@@ -195,15 +229,12 @@ function handleImage(conditionCode, isDay) {
             conditionCode === 1252 ||
             conditionCode === 1117
         ) {
-            img = 'url(./images/snowy.jpg)';
+            img = '/images/snowy.png';
         }
 
-        displayImg();  
+        return img;
 }
-
-function displayImg() {
-    forecastElement.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.1)), ${img}`;   
-} 
+ 
 
 function displayMoonPhaseIcon(phase) {
     const normalizedPhase = phase.toLowerCase().replace(' ', '-');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
   content: ['./src/**/*.{html,js}'],
-  theme: {
-    colors: {
-      ...colors,
-    },
-    extend: {},
-  },
-  plugins: [],
+  darkMode: 'class', 
+  plugins: [], 
 };     
